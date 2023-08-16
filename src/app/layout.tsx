d1bb256537280f017f556ac006a1254780e1cfd5
iivusly <iivusly@icloud.com>
2023-08-16 11:21:32 -0700

    Add @vercel/analytics
    Signed-off-by: iivusly <iivusly@icloud.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Inter } from 'next/font/google';
 import StyledComponentsRegistry from '../lib/AntdRegistry';
+import { Analytics } from '@vercel/analytics/react';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -14,6 +15,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => (
     <html lang="en">
     <body className={inter.className}>
     <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+    <Analytics />
     </body>
     </html>
 );
